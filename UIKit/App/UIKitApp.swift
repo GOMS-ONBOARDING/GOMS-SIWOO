2bbd6bf3687e182feafb34f4f0a529285fd1a59f
Book-on <s40409841@gmail.com>
2026-09-10 16:50:44 +0900

    chore: APP설정 완료

---
+++ UIKit/App/UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitApp.swift
+//  UIKit
+//
+//  Created by 이시우 on 9/10/26.
+//
+
+import UIKit
+
+@main
+struct UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

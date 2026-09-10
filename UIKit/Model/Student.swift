328e1e29bb966fd629600756ee5e6ea3dc2a2c6a
Book-on <s40409841@gmail.com>
2026-09-10 17:29:09 +0900

    feat: SwfitUIKit으로 가져오면서 바꾼 값 주석 처리 및 새로 알게된 부분 주석처리

--- UIKit/Model/Student.swift
+++ UIKit/Model/Student.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Student: Hashable {
+struct Student: Hashable { // Identifiable는 UIKit에서 UITableView를 사용하면 IndexPath로 배열의 데이터를 직접 가져오기 때문에 필수는 아님
     let id: UUID
     let name: String
     let studentNumber: String
@@ -26,7 +26,7 @@ struct Student: Hashable {
     }
 }
 
-enum StudentStatus: String {
+enum StudentStatus: String { // 상태값을 제한하기 위해서 enum으로 오타나 잘못된 상태값을 막을 수 있음
     case inSchool = "교내"
     case outing = "외출"
 }

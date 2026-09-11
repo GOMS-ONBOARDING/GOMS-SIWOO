5fe1052a3cd942756829d8146174c845813e88c9
Book-on <s40409841@gmail.com>
2026-09-11 23:55:17 +0900

    feat: convert StudentDetailView from SwiftUI to UIKit

--- UIKit/Feature/StudentListView/StudentListView.swift
+++ UIKit/Feature/StudentListView/StudentListView.swift
@@ -27,6 +27,7 @@ final class StudentListViewController: UIViewController {
         view.backgroundColor = .systemBackground
 
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.register(StudentListCell.self, forCellReuseIdentifier: StudentListCell.reuseIdentifier)
 
         view.addSubview(tableView)
@@ -54,5 +55,12 @@ extension StudentListViewController: UITableViewDataSource {
 
         cell.configure(with: students[indexPath.row])
         return cell
+    }
+}
+
+extension StudentListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let student = students[indexPath.row]
+        navigationController?.pushViewController(StudentDetailViewController(student: student), animated: true)
     }
 }

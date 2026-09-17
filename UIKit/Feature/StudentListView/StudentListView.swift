f56e2990890a5c8ab4d670a6db4a8d243da89b1c
Book-on <s40409841@gmail.com>
2026-09-17 19:51:47 +0900

    feat: 학생 목록 화면 구현

--- UIKit/Feature/StudentListView/StudentListView.swift
+++ UIKit/Feature/StudentListView/StudentListView.swift
@@ -27,7 +27,6 @@ final class StudentListViewController: UIViewController {
         view.backgroundColor = .systemBackground
 
         tableView.dataSource = self
-        tableView.delegate = self
         tableView.register(StudentListCell.self, forCellReuseIdentifier: StudentListCell.reuseIdentifier)
 
         view.addSubview(tableView)
@@ -55,12 +54,5 @@ extension StudentListViewController: UITableViewDataSource {
 
         cell.configure(with: students[indexPath.row])
         return cell
-    }
-}
-
-extension StudentListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let student = students[indexPath.row]
-        navigationController?.pushViewController(StudentDetailViewController(student: student), animated: true)
     }
 }

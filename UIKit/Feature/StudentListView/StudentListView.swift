4ffdd3a1411307518ed554c53198d441978ff9cd
Book-on <s40409841@gmail.com>
2026-09-20 20:46:40 +0900

    feat: 목록 지각 상태 표시

--- UIKit/Feature/StudentListView/StudentListView.swift
+++ UIKit/Feature/StudentListView/StudentListView.swift
@@ -15,7 +15,7 @@ final class StudentListViewController: UIViewController {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.backgroundColor = .systemBackground
         tableView.separatorStyle = .none
-        tableView.rowHeight = 82
+        tableView.rowHeight = 96
         tableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
         tableView.showsVerticalScrollIndicator = false
         return tableView
--- UIKit/Feature/StudentListView/StudentStatus+Presentation.swift
+++ UIKit/Feature/StudentListView/StudentStatus+Presentation.swift
@@ -5,13 +5,41 @@
 
 import UIKit
 
-extension StudentStatus {
+enum StudentListStatusPresentation {
+    case inSchool
+    case outing
+    case late
+
+    var title: String {
+        switch self {
+        case .inSchool:
+            return "교내"
+        case .outing:
+            return "외출"
+        case .late:
+            return "지각"
+        }
+    }
+
     var badgeBackgroundColor: UIColor {
         switch self {
         case .inSchool:
             return .systemGreen.withAlphaComponent(0.16)
         case .outing:
             return .systemOrange.withAlphaComponent(0.16)
+        case .late:
+            return .systemRed.withAlphaComponent(0.16)
+        }
+    }
+}
+
+extension Student {
+    var listStatusPresentation: StudentListStatusPresentation {
+        switch status {
+        case .inSchool:
+            return .inSchool
+        case .outing:
+            return isLate ? .late : .outing
         }
     }
 }
